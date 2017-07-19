197c8b32605bd37490e6d475881487d6172e69a5
swara-jadhav <swaraonline9@gmail.com>
2017-07-19 18:03:56 +0530

    Third Commit

--- app/src/main/java/karuyaki/com/myapplication/MainActivity.java
+++ app/src/main/java/karuyaki/com/myapplication/MainActivity.java
@@ -2,13 +2,17 @@ package karuyaki.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        txtHello= (TextView) findViewById(R.id.hello);
+        txtHello.setText("LAlit  PAtil");
     }
 }
