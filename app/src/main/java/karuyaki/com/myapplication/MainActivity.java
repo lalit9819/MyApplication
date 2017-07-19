2c6d44238e8fb1ff7f5580814cf591213fa25cce
swara-jadhav <swaraonline9@gmail.com>
2017-07-19 18:09:58 +0530

    Fifth Commit

--- app/src/main/java/karuyaki/com/myapplication/MainActivity.java
+++ app/src/main/java/karuyaki/com/myapplication/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtHello= (TextView) findViewById(R.id.hello);
-        txtHello.setText("Abhijit Patil");
+        txtHello.setText("Patil");
     }
 }
